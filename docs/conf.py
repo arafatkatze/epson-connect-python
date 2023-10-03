5a37ba0c21fb04bc2638e1bd6d182f9ff6c00b78
arafatk <arafat.da.khan@gmail.com>
2023-10-02 23:25:39 -0600

    Adding Docs and fixing .gitignore

--- docs/conf.py
+++ docs/conf.py
@@ -27,5 +27,5 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'nature'
 html_static_path = ['_static']
